f1e4efd4c854c1c964bbc31ccdd3293410a9c035
yao <ywang305@syr.edu>
2021-07-03 01:05:45 -0400

    add twiit

--- src/controller/router.ts
+++ src/controller/router.ts
@@ -1,39 +1,41 @@
-import { Router } from 'https://deno.land/x/oak/mod.ts';
-import { Status } from 'https://deno.land/std/http/http_status.ts';
-import { getPairs, getHistoryKlines, checkTime } from './market.ts';
+import { Router } from "https://deno.land/x/oak/mod.ts";
+import { Status } from "https://deno.land/std/http/http_status.ts";
+import { getPairs, getHistoryKlines, checkTime } from "./market.ts";
+import { getTwitts } from "./twitter.ts";
 
 const books = new Map<string, any>();
-books.set('1', {
-    id: '1',
-    title: 'The Hound of the Baskervilles',
-    author: 'Conan Doyle, Arthur',
+books.set("1", {
+  id: "1",
+  title: "The Hound of the Baskervilles",
+  author: "Conan Doyle, Arthur",
 });
 
 const router = new Router();
 router
-    .get('/', context => {
-        context.response.body = 'Hello Borger!';
-    })
-    .get('/book', context => {
-        context.response.body = Array.from(books.values());
-    })
-    .get(
-        '/book/:id',
-        async (ctx, next) => {
-            const id = ctx.params.id;
-            if (!id || !books.has(id))
-                ctx.throw(Status.Conflict, 'Not Valid Book ID');
-            await next();
-        },
-        (context, next) => {
-            const book = books.get(context?.params?.id ?? '');
-            context.response.body = book;
-        }
-    );
+  .get("/", (context) => {
+    context.response.body = "Hello Borger!";
+  })
+  .get("/book", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get(
+    "/book/:id",
+    async (ctx, next) => {
+      const id = ctx.params.id;
+      if (!id || !books.has(id))
+        ctx.throw(Status.Conflict, "Not Valid Book ID");
+      await next();
+    },
+    (context, next) => {
+      const book = books.get(context?.params?.id ?? "");
+      context.response.body = book;
+    }
+  );
 
 router
-    .get('/exchangeInfo/pairs', getPairs)
-    .get('/klines', getHistoryKlines)
-    .get('/time', checkTime);
+  .get("/exchangeInfo/pairs", getPairs)
+  .get("/klines", getHistoryKlines)
+  .get("/time", checkTime)
+  .get("/api/twitter_search", getTwitts);
 
 export default router;
---
+++ src/controller/twitter.ts
@@ -0,0 +1,62 @@
+import { Context, helpers } from "https://deno.land/x/oak/mod.ts";
+
+export async function getTwitts(
+  context: Context<Record<string, any>>,
+  next: () => Promise<void>
+) {
+  const { lat, lng, radius = 1, lang = "" } = helpers.getQuery(context);
+
+  console.info({ lat, lng, radius, lang });
+
+  const resp = await fetch(
+    "https://api.twitter.com" +
+      `/1.1/search/tweets.json?geocode=${lat},${lng},${radius}km&result_type=recent&count=100&lang=${lang}`,
+    {
+      headers: {
+        Authorization:
+          "Bearer " +
+          "AAAAAAAAAAAAAAAAAAAAAAoRGQEAAAAAVK2dIH5SF3FzIcZOZ8a5IKqZArc%3DxqUez8vakfPyTinWxj8jTqiHTt0G6XF20RkG1e8Vy0MADshDsn",
+      },
+    }
+  );
+  const data = await resp.json();
+
+  console.log("get data: ", data);
+
+  const tweets = data.statuses.map(
+    ({
+      created_at,
+      id,
+      id_str,
+      text,
+      user,
+      place,
+      entities,
+      extended_entities,
+    }: any) => {
+      let media = null;
+      if (
+        extended_entities &&
+        extended_entities.media &&
+        extended_entities.media.length
+      ) {
+        media = extended_entities.media[0];
+      } else if (entities && entities.media && entities.media.length) {
+        media = entities.media[0];
+      }
+
+      return {
+        created_at,
+        id,
+        id_str,
+        text,
+        user_name: user.name,
+        screen_name: user.screen_name,
+        place,
+        media,
+      };
+    }
+  );
+
+  context.response.body = tweets;
+}
